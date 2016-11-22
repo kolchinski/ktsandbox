b2dfca3594273c74bf00ff9fd72ece86617b2c3e
Richard Davis <richard.lee.davis@gmail.com>
2016-11-21 23:42:11 -0800

    Adding a second notebook for project notes, including a todo list.

--- matlab/learn_hmm_params.m
+++ matlab/learn_hmm_params.m
@@ -2,7 +2,7 @@
 M = csvread('../data/python_synthetic.csv');
 trans_ = [0.6,0.4; 
         0,1];
-emis_ = [0.7, 0.3; 
-        0.2, 0.8];
+emis_ = [0.6, 0.4; 
+        0.4, 0.6];
 
 [estTR,estE] = hmmtrain(M,trans_,emis_)
